472730d16fe96877be7011a57921ddc334200776
angelica <abolivar@caleidos.pe>
2025-01-27 11:44:08 -0500

    feat: add alert ec2

--- module/alerts/alerts-ec2.tf
+++ module/alerts/alerts-ec2.tf
@@ -1,14 +1,14 @@
 # CloudWatch Alarm para CPU Utilization
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
-  alarm_name          = "High-CPU-Usage"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 300 # 5 minutos
-  statistic           = "Average"
-  threshold           = 80 # Umbral (80%)
-  alarm_description   = "Alarma cuando el uso de CPU excede el 80% durante 5 minutos."
+  alarm_name          = "${var.ec2_name}-CPU Utilization"
+  comparison_operator = var.cpu_comparison_operator
+  evaluation_periods  = var.cpu_evaluation_periods
+  metric_name         = var.cpu_metric_name
+  namespace           = var.cpu_namespace
+  period              = var.cpu_period # 300 5 minutos
+  statistic           = var.cpu_statistic
+  threshold           = var.cpu_threshold # Umbral 80 (80%)
+  alarm_description   = var.cpu_alarm_description 
   dimensions = {
     InstanceId = var.ec2_instance_id
   }
@@ -21,15 +21,15 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_alarm" {
 }
 # Alarma para uso de Disco
 resource "aws_cloudwatch_metric_alarm" "disk_usage_alarm" {
-  alarm_name          = "High-Disk-Usage"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "DiskSpaceUtilization"
-  namespace           = "CWAgent" # Nombre del namespace del CloudWatch Agent
-  period              = 300 # 5 minutos
-  statistic           = "Average"
-  threshold           = 80 # Umbral (80%)
-  alarm_description   = "Alarma cuando el uso del disco excede el 80% durante 5 minutos."
+  alarm_name          = "${var.ec2_name}-Disk Usage"
+  comparison_operator = var.disk_comparison_operator
+  evaluation_periods  = var.disk_evaluation_periods
+  metric_name         = var.disk_metric_name
+  namespace           = var.disk_namespace  # Nombre del namespace del CloudWatch Agent "CWAgent"
+  period              = var.disk_period # 300 5 minutos
+  statistic           = var.disk_statistic # "Average"
+  threshold           = var.disk_threshold  # 80 Umbral (80%)
+  alarm_description   = var.disk_alarm_description 
   dimensions = {
     InstanceId = var.ec2_instance_id
   }
@@ -38,15 +38,15 @@ resource "aws_cloudwatch_metric_alarm" "disk_usage_alarm" {
 }
 # Alarma para uso de Memoria
 resource "aws_cloudwatch_metric_alarm" "memory_usage_alarm" {
-  alarm_name          = "High-Memory-Usage"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "mem_used_percent"
-  namespace           = "CWAgent" # Nombre del namespace del CloudWatch Agent
-  period              = 300 # 5 minutos
-  statistic           = "Average"
-  threshold           = 80 # Umbral (80%)
-  alarm_description   = "Alarma cuando el uso de la memoria excede el 80% durante 5 minutos."
+  alarm_name          = "${var.ec2_name}-Memory Usage" # "High-Memory-Usage"
+  comparison_operator = var.memory_comparison_operator # "GreaterThanThreshold"
+  evaluation_periods  = var.memory_evaluation_periods # 1
+  metric_name         = var.memory_metric_name # "mem_used_percent"
+  namespace           = var.memory_namespace  # Nombre del namespace del CloudWatch Agent "CWAgent"
+  period              = var.memory_period  # 300 5 minutos
+  statistic           = var.memory_statistic # "Average"
+  threshold           = var.memory_threshold  # 80 Umbral (80%)
+  alarm_description   = var.memory_alarm_description 
   dimensions = {
     InstanceId = var.ec2_instance_id
   }
--- module/alerts/variables.tf
+++ module/alerts/variables.tf
@@ -8,3 +8,135 @@ variable "sns_topic_arn" {
   description = "ARN del SNS Topic existente para notificaciones"
   type        = string
 }
+# Variables generales
+
+variable "ec2_name" {
+  description = "Nombre de la instancia EC2 para identificar las alarmas"
+  type        = string
+}
+
+# CPU Alarm Variables
+variable "cpu_comparison_operator" {
+  description = "Operador de comparación para la alarma de CPU"
+  type        = string
+}
+
+variable "cpu_evaluation_periods" {
+  description = "Cantidad de periodos para evaluar la alarma de CPU"
+  type        = number
+}
+
+variable "cpu_metric_name" {
+  description = "Nombre de la métrica de CPU"
+  type        = string
+}
+
+variable "cpu_namespace" {
+  description = "Namespace de la métrica de CPU"
+  type        = string
+}
+
+variable "cpu_period" {
+  description = "Periodo en segundos para la alarma de CPU"
+  type        = number
+}
+
+variable "cpu_statistic" {
+  description = "Estadística usada en la métrica de CPU"
+  type        = string
+}
+
+variable "cpu_threshold" {
+  description = "Umbral de la métrica de CPU"
+  type        = number
+}
+
+variable "cpu_alarm_description" {
+  description = "Descripción de la alarma de CPU"
+  type        = string
+  default     = "Alarma cuando el uso de CPU excede."
+}
+
+# Disk Alarm Variables
+variable "disk_comparison_operator" {
+  description = "Operador de comparación para la alarma de disco"
+  type        = string
+}
+
+variable "disk_evaluation_periods" {
+  description = "Cantidad de periodos para evaluar la alarma de disco"
+  type        = number
+}
+
+variable "disk_metric_name" {
+  description = "Nombre de la métrica de disco"
+  type        = string
+}
+
+variable "disk_namespace" {
+  description = "Namespace de la métrica de disco"
+  type        = string
+}
+
+variable "disk_period" {
+  description = "Periodo en segundos para la alarma de disco"
+  type        = number
+}
+
+variable "disk_statistic" {
+  description = "Estadística usada en la métrica de disco"
+  type        = string
+}
+
+variable "disk_threshold" {
+  description = "Umbral de la métrica de disco"
+  type        = number
+}
+
+variable "disk_alarm_description" {
+  description = "Descripción de la alarma de disco"
+  type        = string
+  default     = "Alarma cuando el uso del disco excede."
+}
+
+# Memory Alarm Variables
+variable "memory_comparison_operator" {
+  description = "Operador de comparación para la alarma de memoria"
+  type        = string
+}
+
+variable "memory_evaluation_periods" {
+  description = "Cantidad de periodos para evaluar la alarma de memoria"
+  type        = number
+}
+
+variable "memory_metric_name" {
+  description = "Nombre de la métrica de memoria"
+  type        = string
+}
+
+variable "memory_namespace" {
+  description = "Namespace de la métrica de memoria"
+  type        = string
+}
+
+variable "memory_period" {
+  description = "Periodo en segundos para la alarma de memoria"
+  type        = number
+}
+
+variable "memory_statistic" {
+  description = "Estadística usada en la métrica de memoria"
+  type        = string
+}
+
+variable "memory_threshold" {
+  description = "Umbral de la métrica de memoria"
+  type        = number
+}
+
+variable "memory_alarm_description" {
+  description = "Descripción de la alarma de memoria"
+  type        = string
+  default     = "Alarma cuando el uso de la memoria excede."
+}
